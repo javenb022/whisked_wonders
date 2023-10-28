b7d092e1711ef7659d7c7852a3741c37f74ab685
Javen Wilson <javenb022@gmail.com>
2023-10-28 14:18:06 -0400

    Fix attribute validations for user info and email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :invoices
   has_secure_password
-  validates_presence_of :user_name, :password_digest, :role
+  validates_presence_of :password_digest, :role, :first_name, :last_name, :address, :city, :state, :zip
+  validates :email, presence: true, uniqueness: true
 end
