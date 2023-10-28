ba9d5f6ef11b4f9c52ae0669cc6aa0fdae892f55
Javen Wilson <javenb022@gmail.com>
2023-10-28 14:22:07 -0400

    Change user name to email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:user_name) }
+    it { should validate_presence_of(:email) }
     it { should have_secure_password }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:role) }
